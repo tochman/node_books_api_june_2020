93937ad0fe6befadde64374849f421b352b210d2
Thomas <thomas@craftacademy.se>
2020-08-26 11:36:42 +0200

    creates app structure using express-generator-api refactors code to es6 renames resource and endpoint to books

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const books = require('./routes/books');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+
+app.use('/api/v1/books', books);
+
+module.exports = app;
